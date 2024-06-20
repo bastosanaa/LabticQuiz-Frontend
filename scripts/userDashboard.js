453e9014b1e6a5c13ed52db54121dd868dc037fb
Ana <bastosanaa11@gmail.com>
2024-06-20 11:01:35 -0300

    Feat: estado de loading ao entrar na pagina

--- scripts/userDashboard.js
+++ scripts/userDashboard.js
@@ -6,13 +6,24 @@ const studentName = document.getElementById("user-name")
 const subjectsList = document.getElementById("section-content-list")
 const sectionContent = document.querySelector(".section-subject-content")
 const notFoundMessage = document.getElementById("no-data-found-wrapper")
+const loader = document.querySelector(".section-content-loading")
+const sectionContentWrapper = document.getElementById("section-content-wrapper")
 
 // funcao pega user id pelo token
 async function setUserID() {
     const token = localStorage.getItem('token')
     const user_id = await getUserIDByToken(token)
     await setUserDashboard(user_id, token)
+    await esperarUmSegundo()
+    loader.classList.add('hidden')
+    sectionContentWrapper.classList.remove('hidden')
     return user_id
+}
+
+function esperarUmSegundo() {
+    return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+    });
 }
 
 export async function setUserDashboard(user_id, token) {
@@ -22,7 +33,6 @@ export async function setUserDashboard(user_id, token) {
 }
 
 async function setUserNameInGreetings(user_id, token) {
-    console.log(user_id.user, 'AQUIII');
     const user = await getUserbyID(user_id, token)
     const user_name = user.name
     studentName.innerHTML = user_name
