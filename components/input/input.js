b2eecbb35ccdb3cc00fd79aca4add62af8bdaffe
Ana <bastosanaa11@gmail.com>
2024-07-26 12:27:30 -0300

    New Component: Input

---
+++ components/input/input.js
@@ -0,0 +1,73 @@
+export function Input({placeholder,title, required = false, errorMessage = null, type = 'text'}) {
+
+    const inputContainer = document.createElement('div')
+    inputContainer.classList.add('input-container')
+
+    const p = document.createElement('p')
+    p.classList.add('input-label')
+    p.textContent = title
+    inputContainer.appendChild(p)
+
+
+    const input = document.createElement('input')
+    input.setAttribute('type', type)
+    
+    if (placeholder) {
+        input.setAttribute('placeholder', placeholder)
+    }
+    if (required) {
+        input.required = true
+        input.addEventListener('blur', () => {
+            checkIfEmpty(input)}
+        )
+    }
+
+    if (type === 'password') {
+        const togglePassword = document.createElement('img')
+        togglePassword.classList.add('toggle-password')
+        togglePassword.setAttribute('src', 'assets/eye.png' )
+
+        togglePassword.addEventListener('click', () => {
+            togglePasswordStatus(togglePassword)}
+        )
+
+        inputContainer.appendChild(togglePassword)
+    }
+    
+    inputContainer.appendChild(input)
+
+    if (errorMessage) {
+        const p = document.createElement('p')
+        p.classList.add('error-message', 'hidden')
+
+
+        p.textContent = errorMessage
+        inputContainer.appendChild(p)
+    }
+    return inputContainer
+}
+
+function checkIfEmpty(input) {
+    const errorMessage = input.parentNode.querySelector('.error-message')
+        if (input.value == "") {
+            input.style.border = "1px solid red"
+            errorMessage.classList.remove('hidden')
+        } else {
+            input.style.border = ""
+            errorMessage.classList.add('hidden')
+        }
+}
+
+function togglePasswordStatus(togglePassword) {
+    let currentSrc = togglePassword.getAttribute('src')
+    const input = togglePassword.parentNode.querySelector('input')
+
+    if (currentSrc === "assets/eye.png") {
+        togglePassword.setAttribute('src', 'assets/crossed-eye.svg' )
+
+        input.setAttribute('type', 'text')
+        return
+    }
+    togglePassword.setAttribute('src', 'assets/eye.png' )
+    input.setAttribute('type', 'password')
+}
--- componentsCall.js
+++ componentsCall.js
@@ -1,35 +1,22 @@
-import { Button } from "./components/button/button.js";
-import { ContentItem } from "./components/contentItem/contentItem.js";
-import { NavBar } from "./components/navBar/navBar.js";
-import { PageHeader } from "./components/pageHeader/pageHeader.js";
-import { ContentList } from "./components/contentList/contentList.js";
+import { Input } from "./components/input/input.js";
 
-const main = document.getElementById('main')
+const body = document.querySelector('body')
 
-const navBar = NavBar()
-main.appendChild(navBar)
-
-const page = document.createElement('div')
-page.classList.add('page')
-
-const header = PageHeader({
-    title_text: 'Título da página',
-    back_btn: true,
-    subtitle_text: 'subtítulo da página'
+const input = Input({
+    placeholder: 'placeholder',
+    required: true,
+    title: 'Input',
+    errorMessage: 'mensagem de erro',
+    type: 'password'
 })
-page.append(header)
+body.appendChild(input)
 
-const content = ContentList({
-    title_text: 'Conteúdo da lista',
-    content_items: [{
-        text: 'item de lista'
-    },
-    {
-        text: 'item de lista 2'
-    }]
+const input2 = Input({
+    placeholder: 'placeholder',
+
+    title: 'Input2',
+    errorMessage: 'mensagem de erro',
 })
-page.append(content)
 
-
-main.append(page)
+body.appendChild(input2)
 
