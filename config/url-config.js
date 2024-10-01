0d6a3a145a7b55a69a58c8f906bd003d129d428f
Ana <bastosanaa11@gmail.com>
2024-10-01 09:57:46 -0300

    Atualização das urls em producao

--- config/url-config.js
+++ config/url-config.js
@@ -1,4 +1,4 @@
-// export const urlPage = 'https://polvoapp.netlify.app'
-export const urlPage = 'http://127.0.0.1:5501'
-// export const urlBackend = "https://labticquiz-backend.onrender.com/api"
-export const urlBackend = 'http://127.0.0.1:3333/api'
+export const urlPage = 'https://polvoapp.netlify.app'
+// export const urlPage = 'http://127.0.0.1:5501'
+export const urlBackend = "https://labticquiz-backend.onrender.com/api"
+// export const urlBackend = 'http://127.0.0.1:3333/api'
